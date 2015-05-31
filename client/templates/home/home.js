894278f65ca6cc38f842b33268f9816432a52ad5
Spencer Carli <spencercarli@gmail.com>
2015-05-31 15:51:50 -0400

    Capture and process html data on client and convert to js object

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,12 +1,20 @@
 Template.home.events({
-  'change [data-action=upload]': function(e, template) {
+  'change [data-action=upload-xml]': function(e, template) {
+    readFile(e.currentTarget, template);
+  },
+
+  'change [data-action=upload-html]': function(e, template) {
     readFile(e.currentTarget, template);
   }
 });
 
 var readFile = function(input, instance) {
-  var textType = 'text/xml';
+  var textTypeXML = 'text/xml';
+  var textTypeHTML = 'text/html';
   var file;
+
+  var isXML = false;
+  var isHTML = false;
 
   if (input.files && input.files[0]) {
     file = input.files[0];
@@ -15,7 +23,10 @@ var readFile = function(input, instance) {
     return;
   }
 
-  if (file.type === textType) {
+  if (file.type === textTypeXML) isXML = true;
+  if (file.type === textTypeHTML) isHTML = true;
+
+  if (isXML || isHTML) {
     var FR = new FileReader;
 
     FR.onload = function(e) {
@@ -23,20 +34,71 @@ var readFile = function(input, instance) {
       var data = FR.result;
 
       if (data) {
-        Meteor.call('File.process', data, function(err, res) {
-          if (err) {
-            Helpers.notify(err.reason);
-          } else {
-            console.log(res);
-          }
+        if (isXML) {
+          Meteor.call('File.processXML', data, function(err, res) {
+            if (err) {
+              Helpers.notify(err.reason);
+            } else {
+              console.log(res);
+            }
+            input.value = '';
+          });
+        } else if (isHTML) {
+          var obj = readHTML(data);
+          Meteor.call('File.processHTMLObj', obj, function(err, res) {
+
+          });
           input.value = '';
-        });
+        }
       }
     };
 
     FR.readAsText(file);
   } else {
-    Helpers.notify("File must be .xml");
+    Helpers.notify("File must be .xml or .html");
+    input.value = '';
     return;
+  }
+};
+
+var readHTML = function(data) {
+  var $html = $.parseHTML(data);
+  $('#temp-data').append($html);
+
+  var data = [];
+  var $table = $('[name=closed_trades]');
+  var headers = getHeaders($table);
+
+  $table.find('tr').each(function() {
+    var rowData = getRowData(headers, this);
+    if (rowData) {
+      data.push(rowData);
+    }
+  });
+  return data;
+};
+
+var getHeaders = function($table) {
+  var headers = [];
+
+  $table.find('.header').each(function() {
+    headers.push($(this).text());
+  });
+  return headers;
+};
+
+var getRowData = function(headers, tr) {
+  var data = $(tr).find('td');
+  if (headers.length === data.length) {
+    var cellOne = data.first().text();
+
+    if (_.contains(headers, cellOne)) return;
+
+    var d = {};
+    _.each(headers, function(header, i) {
+      d[header] = $(data[i]).text();
+    })
+
+    return d;
   }
 };
--- server/methods/file.js
+++ server/methods/file.js
@@ -1,11 +1,19 @@
 Meteor.methods({
-  'File.process': function(data) {
+  'File.processXML': function(data) {
     xml2js.parseString(data, {}, function(err, res) {
       console.log('err', err);
+      if (err) {
+        throw new Meteor.Error(400, "An error occured.");
+      }
       // console.log('res', JSON.stringify(res));
-      var data = JSON.stringify(res['ss:Workbook']['ss:Worksheet'][0]['ss:Table']);
-      console.log(data);
+      var data = res['ss:Workbook']['ss:Worksheet'][0]['ss:Table'];
+      console.log(JSON.stringify(data[0]['ss:Row'][30]));
     });
+    return 'Response';
+  },
+
+  'File.processHTMLObj': function(data) {
+    console.log(JSON.stringify(data));
     return 'Response';
   }
 });
